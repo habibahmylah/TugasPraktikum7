f00a1fb5c49cae063303fb86f19b1fa249a9890c
habibahmylah <100356740+habibahmylah@users.noreply.github.com>
2022-04-21 08:26:48 +0700

    Add files via upload

---
+++ BinSearch1.h
@@ -0,0 +1,15 @@
+/*Nama File : BinSearch.h*/
+/*Nama : Habibah Mylah Dalilah*/
+/*NIM : 24060121120028*/
+/*Tanggal Selesai : 20 April 2022*/
+
+#ifndef BINSEARCH1_H_INCLUDED
+#define BINSEARCH1_H_INCLUDED
+
+void BinSearch1(int T[], int N, int X, int *IX);
+/*Mencari nilai X pada sebuah tabel T menggunakan binary search, apabila X tidak terdapat pada tabel T
+maka keluarannya adalah elemen tidak ditemukan. Sedangkan apabila X terdapat pada
+tabel T maka keluarannya adalah elemen ditemukan pada indeks : ix dan pada urutan
+ke ix+1*/
+
+#endif // BINSEARCH1_H_INCLUDED
